8f6123fd86798560f02ccecceb6703ef181b6978
Jeff Martin <jmartin@cleardata.com>
2016-05-07 15:03:00 -0700

    added deck stuff to main library

---
+++ test/decktests.js
@@ -0,0 +1,78 @@
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var chaiAsPromised = require("chai-as-promised"),
+    shuffle = require('../lib/deck');
+var should =chai.should();
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+
+var rnd = function() { return 0.25; }; // de-randomizing
+
+describe('shuffle', function() {
+    it('draw() works', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        var card = deck.draw();
+        should.not.exist(card.length);
+        card.toShortDisplayString().should.equal('2D');
+        deck.length.should.equal(51);
+    });
+
+    it('draw(2) works', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        var cards = deck.draw(2);
+        should.exist(cards.length);
+        cards.length.should.equal(2);
+        cards[0].toShortDisplayString().should.equal('2D');
+        cards[1].toShortDisplayString().should.equal('AC');
+        deck.length.should.equal(50);
+    });
+
+    it('draw more than deck errors', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        var cards = deck.draw(52);
+        should.exist(cards.length);
+        cards.length.should.equal(2);
+        cards[0].toShortDisplayString().should.equal('2D');
+        cards[1].toShortDisplayString().should.equal('AC');
+        deck.length.should.equal(50);
+    });
+
+    it('drawRandom() works', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        var card = deck.drawRandom();
+        should.not.exist(card.length);
+        card.toShortDisplayString().should.equal('3D');
+        deck.length.should.equal(51);
+    });
+
+    it('drawRandom(2) works', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        var cards = deck.drawRandom(2);
+        should.exist(cards.length);
+        cards.length.should.equal(2);
+        cards[0].toShortDisplayString().should.equal('3D');
+        cards[1].toShortDisplayString().should.equal('AS');
+        deck.length.should.equal(50);
+    });
+
+    it('reset works', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        var card = deck.draw();
+        card.toShortDisplayString().should.equal('2D');
+        deck.length.should.equal(51);
+
+        deck.reset();
+        card = deck.draw();
+        card.toShortDisplayString().should.equal('AS');
+        deck.length.should.equal(51);
+    });
+
+    it('issue #3', function() {
+        var deck = shuffle.shuffle({ random: rnd });
+        deck.length.should.equal(52);
+        var card = deck.drawFromBottomOfDeck(1);
+        deck.length.should.equal(51);
+    });
+});
---
+++ test/playertests.js
@@ -0,0 +1,29 @@
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.should();
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+var Player = require('../lib/player');
+var Game = require("../lib/game");
+
+describe("player", function() {
+    describe("initialDraw", function() {
+        it("draws 7 cards into players hand", function() {
+            var player1 = new Player({
+                cards: [
+                    { name: "Wastes" },
+                    { name: "Wastes" },
+                    { name: "Wastes" },
+                    { name: "Wastes" },
+                    { name: "Wastes" }
+                ]
+            });
+            var game = new Game([player1]);
+            //game sets up library and emits event calling initialDraw    
+
+            player1.hand.length.should.equal(7);
+        });
+    });
+});
